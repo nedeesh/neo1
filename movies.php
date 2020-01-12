bdc7c45667fd9548e9ff56506defa6645dc04220
nedeesh <nedeeshmuralinithu007@gmail.com>
2020-01-12 15:38:52 +0530

    movie response updated

--- movies.php
+++ movies.php
@@ -18,43 +18,67 @@
     <title>Document</title>
 </head>
 <body>
+    <form method=GET>
     <table class="table">
         <TR>
             <td>Movie name</td>
-            <td><input type="text" class="form-control"></td>
+            <td><input type="text" class="form-control" name="getMovie"></td>
         </TR>
         <tr>
             <td>Actor name</td>
-            <td><input type="text" class="form-control"></td>
+            <td><input type="text" class="form-control" name="getActor"></td>
         </tr>
         <tr>
             <td>Actress</td>
-            <td><input type="text" class="form-control"></td>
+            <td><input type="text" class="form-control" name="getActress"></td>
         </tr>
         <tr>
             <td>Director</td>
-            <td><input type="text" class="form-control"></td>
+            <td><input type="text" class="form-control"name="getDirector"></td>
         </tr>
         <tr>
             <td>Camera</td>
-            <td><input type="text" class="form-control"></td>
+            <td><input type="text" class="form-control"name="getCamera"></td>
         </tr>
         <tr>
             <td>Producer</td>
-            <td><input type="text" class="form-control"></td>
+            <td><input type="text" class="form-control"name="getProducer"></td>
         </tr>
         <tr>
             <td>Distributor</td>
-            <td><input type="text" class="form-control"></td>
+            <td><input type="text" class="form-control"name="getDistributer"></td>
         </tr>
         <tr>
             <td>Released date</td>
-            <td><input type="text" class="form-control"></td>
+            <td><input type="text" class="form-control"name="getReleased"></td>
         </tr>
         <tr>
-            <td><button class="btn btn-danger">Reset</button></td>
-            <td><button class="btn btn-danger">Submit</button></td>
+            <td><button class="btn btn-danger"name="getReset">Reset</button></td>
+            <td><button class="btn btn-danger"name="getSubmit">Submit</button></td>
         </tr>
     </table>
+</form>
 </body>
 </html>
+<?php
+if(isset($_GET["getSubmit"]))
+{
+    $actor=$_GET["getActor"];
+    $movie=$_GET["getMovie"];
+    $actress=$_GET["getActress"];
+    $director=$_GET["getDirector"];
+    $camera=$_GET["getCamera"];
+    $producer=$_GET["getProducer"];
+    $distributer=$_GET["getDistributer"];
+    $released=$_GET["getReleased"];
+    echo $actor;
+    echo $movie;
+    echo $actress;
+    echo $director;
+    echo $camera;
+    echo $producer;
+    echo $distributer;
+    echo $released;
+
+}
+?>
